b0a2a4451c091a30448dab86cfd0c32378411373
Pedro <pedroaraujo@morethanreal.io>
2020-05-05 15:20:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
-const ejs = require("ejs");
-const bodyParser = require('body-parser');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
-var port = process.env.PORT || 3000;
-app.listen(port);
-
-app.use(bodyParser.urlencoded({extended: true})); 
-app.set("view engine", "ejs");
+const ejs = require("ejs");
+const bodyParser = require('body-parser');
 
 http.listen(process.env.PORT || 8080, () => {
   console.log('listening on *:'+8080);
 });
+
+
+
+
+app.use(bodyParser.urlencoded({extended: true})); 
+app.set("view engine", "ejs");
+
+
 
 
 io.on('connection', (socket) => {
