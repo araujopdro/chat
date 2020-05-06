b78da516320a917ff25ff32ee8f452fb1979c325
Pedro <pedroaraujo@morethanreal.io>
2020-05-06 16:27:27 -0300

    asdasd

--- index.js
+++ index.js
@@ -9,38 +9,49 @@ http.listen(process.env.PORT || 8080, () => {
   console.log('listening on *:'+8080);
 });
 
-
-
+var chat_history = [];
+var current_users = [];
 
 app.use(bodyParser.urlencoded({extended: true})); 
 app.set("view engine", "ejs");
 
-
-
-
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log(socket.id+' connected');
+  socket.broadcast.emit('user connected', socket.handshake.query.name);
+  io.to(socket.id).emit('chat history', {ch: chat_history});
 
+  AddToChatHistory(socket.handshake.query.name);
+  current_users.push({"id":socket.id, "name":socket.handshake.query.name});
+  
   socket.on('chat message', (data) => {
-    console.log(data);
+    AddToChatHistory(data);
     io.emit('chat message', data);
   });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('disconnect', (reason) => {
+    console.log(socket.id+' disconnected');
+    console.log(current_users);
+    var index = current_users.findIndex(x => x.id === socket.id);
+    console.log(index);
+    socket.broadcast.emit('user disconnected', current_users[index].name);
+   // current_users.splice(index);
+    console.log(reason);
   });
 });
 
-
 ///
-
 app.get('/', (req, res) => {
   res.render("login");
 });
 
-var user_data;
+var latest_user;
 app.post("/chat-room", (req, res) => {
-  console.log(req);
+  latest_user = req.body.fname + " " + req.body.lname;
   res.render("chat", req.body);
 });
 
+///
+function AddToChatHistory(e){
+    chat_history.push(e);
+    if(chat_history.length > 199){chat_history.splice(0)};
+}
