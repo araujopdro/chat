54d4ca7c9dbb10370887812837e2d5d1462b724e
Pedro <pedroaraujo@morethanreal.io>
2020-05-19 19:37:52 -0300

    image public fix

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(express.static(__dirname + '/public'));
 
 
 io.on('connect', (socket) => {
